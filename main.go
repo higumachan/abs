5a0a041329c25bad7d42ea94111f219b4b9fe1ee
higumachan <harekumo792154@gmail.com>
2019-10-02 17:56:09 +0900

    not found

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
 
 func main() {
 	path := os.Args[1]
+
+	_, err2 := os.Stat(path)
+	if os.IsNotExist(err2) {
+		fmt.Printf("abs: \"%s\": No such file or directory\n", path)
+		return
+	}
+
 	path, err := filepath.Abs(path)
 	if err != nil {
 		println(err.Error())
 		return
 	}
+
 	println(path)
 }
